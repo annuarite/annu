4516fa6a714e68af031c25a5240051f0838fe121
annuarite <105701127+annuarite@users.noreply.github.com>
2022-05-20 11:18:46 +0300

    Update calc.py

--- calc.py
+++ calc.py
@@ -10,5 +10,4 @@ division_of_nums = f_num/s_num
 
 print("the division of the two numbers is"+str(division_of_nums))
 difference_btwn_nums = s_num-f_num
-
 print("the difference btwn the two nums is",(difference_btwn_nums)) 
