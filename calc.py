e62fc16a9e2458e1021da30cced06dd8d0b09490
annuarite <105701127+annuarite@users.noreply.github.com>
2022-05-20 11:03:16 +0300

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,14 @@
+f_num = float(input("input the first number:"))
+s_num = float(input("input the second number:"))
+sum_of_nums = f_num+s_num
+
+print("the sum of the two numbers is"+str(sum_of_nums))
+product_of_nums = f_num*s_num
+
+print("the product of the two nums is"+str(product_of_nums))
+division_of_nums = f_num/s_num
+
+print("the division of the two numbers is"+str(division_of_nums))
+difference_btwn_nums = s_num-f_num
+
+print("the difference btwn the two nums is",(difference_btwn_nums)) 
